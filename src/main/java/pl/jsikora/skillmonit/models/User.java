6fd9b81506bbe372b5c902dd070dfc1a60607a27
js <grawires@gmail.com>
2022-02-18 00:38:09 +0100

    User table and database preparation

--- src/main/java/pl/jsikora/skillmonit/models/User.java
+++ src/main/java/pl/jsikora/skillmonit/models/User.java
@@ -1,8 +1,14 @@
 package pl.jsikora.skillmonit.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
 public class User {
 
+    @Id
+    String id;
     String username;
     String password;
     String firstname;
