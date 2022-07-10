28225ba71293bafa1318a65422d2232c0c718410
jobLaptok <grawires@gmail.com>
2022-07-10 11:37:51 +0200

    test fails to run - spring context, h2 db, liquibase

--- src/test/java/pl/jsikora/skillmonit/SkillmonitApplicationTests.java
+++ src/test/java/pl/jsikora/skillmonit/SkillmonitApplicationTests.java
@@ -1,9 +1,16 @@
 package pl.jsikora.skillmonit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
 class SkillmonitApplicationTests {
 
     @Test
--- src/test/java/pl/jsikora/skillmonit/service/UserServiceTest.java
+++ src/test/java/pl/jsikora/skillmonit/service/UserServiceTest.java
@@ -1,46 +1,56 @@
 package pl.jsikora.skillmonit.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
+import pl.jsikora.skillmonit.dto.UserDTO;
 import pl.jsikora.skillmonit.dto.UserRegisterDTO;
 import pl.jsikora.skillmonit.mapper.UserMapper;
 import pl.jsikora.skillmonit.models.User;
 import pl.jsikora.skillmonit.models.UserRepository;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
+@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
-    @Mock
+    @MockBean
     private UserRepository userRepository;
 
-    @Mock
+    @SpyBean
     private UserMapper userMapper;
 
-    @Mock
-    private User USER_MOCK;
-
-    @Mock
+    @MockBean
     private UserRegisterDTO USER_REGISTER_MOCK;
 
-    @InjectMocks
+    @Autowired
     private UserService userService;
 
     @Test
-    void addUser_should_add_user(){
-        //given
-        //UserRegisterDTO testUser = userMapper.map new User(USER_MOCK);
+    void addUser_should_add_user() {
+        // given
+        UserRegisterDTO testUserRequest = new UserRegisterDTO();
+        testUserRequest.setUsername("username");
+        testUserRequest.setEmail("email");
+        testUserRequest.setPassword("password");
+        testUserRequest.setFirstname("firstname");
+        testUserRequest.setLastname("lastname");
 
-        //when
-        when( userRepository.findByUsername(USER_REGISTER_MOCK.getUsername()) ).thenReturn(null);
+        // when
+        when(userRepository.findByUsername(testUserRequest.getUsername())).thenReturn(null);
+        //when(userMapper.mapToUser(testUserRequest)).thenReturn(testUserRequest);
+        UserDTO expectedUser = userService.addUser(testUserRequest);
 
-        //then
-        a
+        // then
+        assertEquals(expectedUser.getUsername(), testUserRequest.getUsername());
     }
 }
