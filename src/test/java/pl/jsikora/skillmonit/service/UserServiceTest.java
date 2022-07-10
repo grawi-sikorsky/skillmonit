96d29aa7fda73d8ac699c0bf9ff532d4ce4fba0b
jobLaptok <grawires@gmail.com>
2022-07-10 13:35:17 +0200

    tests passed

--- src/test/java/pl/jsikora/skillmonit/service/UserServiceTest.java
+++ src/test/java/pl/jsikora/skillmonit/service/UserServiceTest.java
@@ -1,12 +1,15 @@
 package pl.jsikora.skillmonit.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,28 +32,52 @@ public class UserServiceTest {
     @SpyBean
     private UserMapper userMapper;
 
-    @MockBean
-    private UserRegisterDTO USER_REGISTER_MOCK;
-
     @Autowired
     private UserService userService;
 
+    private User USER_MOCK;
+    private UserRegisterDTO USER_REGISTER_MOCK;
+
+    @BeforeEach
+    void prepare(){
+        USER_REGISTER_MOCK = new UserRegisterDTO();
+        USER_REGISTER_MOCK.setUsername("username");
+        USER_REGISTER_MOCK.setEmail("email");
+        USER_REGISTER_MOCK.setPassword("password");
+        USER_REGISTER_MOCK.setFirstname("firstname");
+        USER_REGISTER_MOCK.setLastname("lastname");
+
+        USER_MOCK = new User();
+        USER_MOCK.setUsername("username");
+        USER_MOCK.setEmail("email");
+        USER_MOCK.setPassword("password");
+        USER_MOCK.setFirstname("firstname");
+        USER_MOCK.setLastname("lastname");
+        USER_MOCK.setUuid("uuid");
+        USER_MOCK.setPhone("phone");
+    }
+
     @Test
-    void addUser_should_add_user() {
+    void addUser_should_save_user() {
         // given
-        UserRegisterDTO testUserRequest = new UserRegisterDTO();
-        testUserRequest.setUsername("username");
-        testUserRequest.setEmail("email");
-        testUserRequest.setPassword("password");
-        testUserRequest.setFirstname("firstname");
-        testUserRequest.setLastname("lastname");
+        User userToSave = userMapper.mapToUser(USER_REGISTER_MOCK);
 
         // when
-        when(userRepository.findByUsername(testUserRequest.getUsername())).thenReturn(null);
-        //when(userMapper.mapToUser(testUserRequest)).thenReturn(testUserRequest);
-        UserDTO expectedUser = userService.addUser(testUserRequest);
+        when(userRepository.save(any(User.class))).thenReturn(userToSave);
+        UserDTO expectedUserDTO = userService.addUser(USER_REGISTER_MOCK);
 
         // then
-        assertEquals(expectedUser.getUsername(), testUserRequest.getUsername());
+        assertEquals(USER_REGISTER_MOCK.getUsername(), expectedUserDTO.getUsername());
+        verify(userRepository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    void addUser_should_throw_exception_when_user_exists() {
+
+        when(userRepository.findByUsername(USER_REGISTER_MOCK.getUsername())).thenReturn(USER_MOCK);
+
+        assertThrows(RuntimeException.class, () -> {
+            userService.addUser(USER_REGISTER_MOCK);
+        });
     }
 }
