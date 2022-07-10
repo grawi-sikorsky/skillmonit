caecf47b5efa11d21d312b415df68fc9c628af9a
jobLaptok <grawires@gmail.com>
2022-07-10 15:48:15 +0200

    integration test start

---
+++ src/test/java/pl/jsikora/skillmonit/controller/UserControllerTest.java
@@ -0,0 +1,13 @@
+package pl.jsikora.skillmonit.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class UserControllerTest {
+    
+}
